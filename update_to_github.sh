32e06ec239b45cafe210e0bf804068fd129ef2da
liz <93873637@qq.com>
2020-07-15 15:27:20 +0800

    update_200715.152719

---
+++ update_to_github.sh
@@ -0,0 +1,25 @@
+#!/bin/bash 
+
+echo ""
+echo "***update_to_github: enter..."
+echo ""
+
+TIME_FLAG=`date +"%y%m%d.%H%M%S"`
+
+run_git_cmd(){
+    #echo "run_git_cmd..."
+    echo ""
+    echo "# $*"
+    $*
+    echo ""
+}
+
+run_git_cmd "git status"
+run_git_cmd "git add ."
+run_git_cmd "git commit -m update_${TIME_FLAG}"
+run_git_cmd "git push git@github.com:93873637/PureTorch.git"
+run_git_cmd "git status"
+
+echo ""
+echo "***update_to_github: exit."
+echo ""
